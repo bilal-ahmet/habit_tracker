4fc6e013d340b49cad7c1f138ffb846148eae322
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-07-30 18:58:49 +0300

    theme_settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/pages/home_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
---
+++ lib/theme/theme_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/theme/dark_mode.dart';
+import 'package:habit_tracker/theme/light_mode.dart';
+
+class ThemeProvider extends ChangeNotifier{
+
+  // initially light mode
+  ThemeData _themeData = lightMode;
+
+  // get current theme
+  ThemeData get themeData => _themeData;
+
+  //is current theme dark mode
+  bool get isDarkMode => _themeData == darkMode;
+
+  //set theme
+  set themeData(ThemeData themeData){
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  //toggle theme
+  void toggleTheme(){
+    if(_themeData == lightMode){
+      themeData = darkMode;
+    }
+    else{
+      themeData = lightMode;
+    }
+  }
+}
