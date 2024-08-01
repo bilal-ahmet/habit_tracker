6da6dffa73bdebb405c4e0b06f586522e5569b06
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-08-01 18:20:03 +0300

    decided_to_database_roadmap

---
+++ lib/database/habit_databse.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:isar/isar.dart';
+
+class HabitDatabse extends ChangeNotifier {
+  static late Isar isar;
+
+  /*
+  SETUP
+  */
+
+  //  INITIALIZE - DATABASE
+
+  //  save first date of app startup (for heatmap)
+
+  //  get first date of app startup (for heatmap)
+
+  /*
+  CRUD X OPERATIONS
+  */
+
+  //  List of habits
+
+  //  CREATE - add a new habbits
+
+  //  READ - read saved habbits from db
+
+  //  UPDATE - check habbit on and off
+
+  //  UPDATE - edit habbit name
+
+  //  DELETE - delete habbits
+}
